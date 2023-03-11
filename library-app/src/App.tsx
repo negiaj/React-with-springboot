7d95e821804ebfec8417fe85610fdc5bec806b33
imajaynegi <ajaynegi61@gmail.com>
2023-03-12 02:54:51 +0530

    update ui

--- library-app/src/App.tsx
+++ library-app/src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Navbar from './layouts/Navbar/Navbar';
 import ExploreTopBooks from './layouts/HomePage/ExploreTopBooks';
 import Carousel from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
+import { LibraryServices } from './layouts/HomePage/LibraryServices';
+import { Footer } from './layouts/HomePage/Footer';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
       <Navbar/>
       <ExploreTopBooks/>
       <Carousel/>
+      <Heros/>
+      <LibraryServices/>
+      <Footer/>
     </div>
   );
 }
